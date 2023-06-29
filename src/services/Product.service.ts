df1dfd62147eaee4a514c8ddb92e23ac5a0e3b4f
Sebastian Bran <sebastianbranzapata@gmail.com>
2023-06-29 14:41:20 -0500

    [feature/services]: fix services

--- src/services/Product.service.ts
+++ src/services/Product.service.ts
@@ -1,29 +1,29 @@
-import axios from "axios";
 import CreateProduct from "../models/product/CreateProduct.model";
 import UpdateProduct from "../models/product/UpdateProduct.model";
 import getRandomId from "../utils/IdGenerator";
+import http from "./http-common";
 
 class ProductsService {
   private BASE_URL = "/products";
 
   public getAll() {
-    return axios.get(this.BASE_URL);
+    return http.get(this.BASE_URL);
   }
 
   public getById(sku: number) {
-    return axios.get(`${this.BASE_URL}/${sku}`);
+    return http.get(`${this.BASE_URL}/${sku}`);
   }
 
   public create(data: CreateProduct) {
-    return axios.post(this.BASE_URL, { sku: getRandomId, ...data });
+    return http.post(this.BASE_URL, { sku: getRandomId, ...data });
   }
 
   public update(sku: number, data: UpdateProduct) {
-    return axios.post(`${this.BASE_URL}/${sku}}`, data);
+    return http.post(`${this.BASE_URL}/${sku}}`, data);
   }
 
   public delete(sku: number) {
-    return axios.post(`${this.BASE_URL}/${sku}}`);
+    return http.post(`${this.BASE_URL}/${sku}}`);
   }
 }
 
