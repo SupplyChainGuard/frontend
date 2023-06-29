b89bae1976dd9f6a60b8c8480bd8e4a6182b8dea
jsroma <joseluis.rome@protonmail.com>
2023-06-29 13:51:06 -0500

    feat: add table with style and default values

--- src/pages/auth-views/providers/Providers.tsx
+++ src/pages/auth-views/providers/Providers.tsx
@@ -7,7 +7,7 @@ function Providers() {
       <header className="my-2 mx-6 flex justify-end">
         <AddButton title="Add Provider" />
       </header>
-      <main className="flex justify-center">
+      <main className="flex justify-center mx-4">
         <Table />
       </main>
     </div>
--- src/pages/auth-views/shared/components/Table.tsx
+++ src/pages/auth-views/shared/components/Table.tsx
@@ -1,7 +1,36 @@
 function Table() {
+  const data = [
+    { id: 1, name: "Item 1", category: "Category A", status: "Active" },
+    { id: 2, name: "Item 2", category: "Category B", status: "Inactive" },
+    { id: 3, name: "Item 3", category: "Category C", status: "Active" },
+  ];
+
   return (
-    <div>Table</div>
-  )
+    <table className="w-full bg-gray-200">
+      <thead>
+        <tr className="bg-gray-400">
+          <th className="p-2">ID</th>
+          <th className="p-2">Name</th>
+          <th className="p-2">Category</th>
+          <th className="p-2">Status</th>
+          <th className="p-2">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item) => (
+          <tr key={item.id} className="bg-white">
+            <td className="p-2 text-center">{item.id}</td>
+            <td className="p-2 text-center">{item.name}</td>
+            <td className="p-2 text-center">{item.category}</td>
+            <td className="p-2 text-center">{item.status}</td>
+            <td className="p-2 text-center">
+              <span className="text-blue-500">🛠️</span>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
-export default Table
+export default Table;
