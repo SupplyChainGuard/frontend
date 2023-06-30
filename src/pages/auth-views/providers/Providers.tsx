8f364b535c5ec9537d9098646a7c87f6010671da
jsroma <joseluis.rome@protonmail.com>
2023-06-29 20:51:08 -0500

    feat: add table getProviders and update add button

--- src/pages/auth-views/providers/Providers.tsx
+++ src/pages/auth-views/providers/Providers.tsx
@@ -1,24 +1,35 @@
 import AddButton from "../shared/components/AddButton";
 import Table from "../shared/components/Table";
+import useFetchProviders from "./hooks/useGetProviders";
 
 function Providers() {
-  const headers = ["ID", "Name", "Category", "Status", "Action"];
-  const data = [
-    { id: 1, values: ["1", "Item 1", "Category A", "Active"] },
-    { id: 2, values: ["2", "Item 2", "Category B", "Inactive"] },
-    { id: 3, values: ["3", "Item 3", "Category C", "Active"] },
-  ]
+
+  const { providers } = useFetchProviders();
+
   const dataAction = () => {
     console.log("hola action");
+  };
+
+  const tableProps = {
+    headers: ["ID", "Name", "Category", "Status", "Action"],
+    data: providers?.map((provider) => ({
+      id: provider.id,
+      values: [
+        provider.id.toString(),
+        provider.name,
+        provider.category,
+        provider.status.toString(),
+      ],
+    })) || [],
   }
 
   return (
     <div>
       <header className="my-2 mx-6 flex justify-end">
-        <AddButton title="Add Provider" />
+        <AddButton action={dataAction} title="Add Provider" />
       </header>
       <main className="flex justify-center mx-4">
-        <Table headers={headers} data={data} action={dataAction} />
+        <Table headers={tableProps.headers} data={tableProps.data} action={dataAction} />
       </main>
     </div>
   );
