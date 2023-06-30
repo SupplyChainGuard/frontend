bd902f8a419e8c8052c1150d6a518badeabfe95b
JorgEsco98 <91279499+JorgEsco98@users.noreply.github.com>
2023-06-30 02:54:34 -0500

    Update useCreateProduct.tsx

--- src/pages/auth-views/inventory/hooks/useCreateProduct.tsx
+++ src/pages/auth-views/inventory/hooks/useCreateProduct.tsx
@@ -1,1 +1,27 @@
-hola
+import CreateProduct from "../../../../models/product/CreateProduct.model";
+import {useState} from "react";
+import ProductService from "../../../../services/Product.service";
+
+interface CreateProductProps {
+    product?:CreateProduct;
+}
+
+function useCreateProduct(){
+    const [loadingCreateProduct, setLoadingCreateProduct]=useState<boolean>(false)
+    const [errorCreateProduct, setErrorCreateProduct]=useState<string|null>(null)
+
+    const createProduct = async (product:CreateProduct)=>{
+        setLoadingCreateProduct(true);
+        if(product){
+            await ProductService.create(product)
+                .then()
+                .catch((error)=>{
+                    setErrorCreateProduct(error.message);
+                });
+        }
+        setLoadingCreateProduct(false);
+    };
+    return {loadingCreateProduct,errorCreateProduct,createProduct};
+}
+
+export default useCreateProduct;
