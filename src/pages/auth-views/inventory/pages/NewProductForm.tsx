bb93a67e2e78b6638fe51f936ce045695ea42f62
JorgEsco98 <91279499+JorgEsco98@users.noreply.github.com>
2023-06-30 02:54:07 -0500

    Update NewProductForm.tsx

--- src/pages/auth-views/inventory/pages/NewProductForm.tsx
+++ src/pages/auth-views/inventory/pages/NewProductForm.tsx
@@ -1,66 +1,88 @@
-import { useState } from "react";
-import CreateShipment from "../../../../models/shipment/CreateShipment.model";
-import useCreateShipment from "../hooks/useCreateShipment";
+import React, {useState} from "react";
+import CreateProduct from "../../../../models/product/CreateProduct.model";
+import {useNavigate} from "react-router";
 import Input from "../../../shared/components/Input";
-import { useNavigate } from "react-router";
+import useCreateProduct from "../../inventory/hooks/useCreateProducts";
 
-function NewShipment() {
-  const navigate = useNavigate();
-  const [shipment, setShipment] = useState<CreateShipment>(
-    {} as CreateShipment
-  );
-  const { loadingCreateShipment, errorCreateShipment, createShipment } =
-    useCreateShipment();
+function NewProduct() {
+    const navigate = useNavigate();
+    const [product, setProduct] = useState<CreateProduct>(
+        {} as CreateProduct
+    );
+    const {loadingCreateProduct, errorCreateProduct, createProduct} =
+        useCreateProduct();
 
-  const handleOnSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    await createShipment({ ...shipment, date: new Date() });
-    if (!errorCreateShipment) {
-      alert("Shipment created successfully");
-      navigate("/auth/shipments");
-    }
-  };
+    const handleOnSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        await createProduct({...product});
+        if (!errorCreateProduct) {
+            alert("Product created successfully");
+            navigate("/auth/inventory");
+        }
+    };
 
-  return (
-    <div className="p-4">
-      <h1 className="text-3xl font-bold">New Shipment</h1>
-      <form onSubmit={handleOnSubmit}>
-        <div className="grid grid-cols-2 gap-4 mt-6">
-          <Input
-            name="product"
-            labelName="Product SKU"
-            value={shipment.productSKU || ""}
-            onChange={(e) => {
-              setShipment({
-                ...shipment,
-                productSKU: +e.target.value,
-              });
-            }}
-          />
-          <Input
-            name="quantity"
-            labelName="Quantity"
-            value={shipment.quantity || ""}
-            onChange={(e) => {
-              setShipment({
-                ...shipment,
-                quantity: +e.target.value,
-              });
-            }}
-          />
+
+    return (
+        <div className="p-4">
+            <h1 className="text-3xl font-bold">New Product</h1>
+            <form onSubmit={handleOnSubmit}>
+                <div className="grid grid-cols-2 gap-4 mt-6">
+                    <Input
+                        name="name"
+                        labelName="Product Name"
+                        value={product.name || ""}
+                        onChange={(e) => {
+                            setProduct({
+                                ...product,
+                                name: e.target.value,
+                            });
+                        }}
+                    />
+                    <Input
+                        name="category"
+                        labelName="Category"
+                        value={product.category || ""}
+                        onChange={(e) => {
+                            setProduct({
+                                ...product,
+                                category: e.target.value,
+                            });
+                        }}
+                    />
+                    <Input
+                        name="stock"
+                        labelName="Stock"
+                        value={product.stock || ""}
+                        onChange={(e) => {
+                            setProduct({
+                                ...product,
+                                stock: +e.target.value,
+                            });
+                        }}
+                    />
+                    <Input
+                        name="status"
+                        labelName="Status"
+                        value={product.status || ""}
+                        onChange={(e) => {
+                            setProduct({
+                                ...product,
+                                status: +e.target.value,
+                            });
+                        }}
+                    />
+                </div>
+                <div className="flex mt-6">
+                    <button className="btn-secondary mr-4 ml-auto" type="submit"
+                            onClick={() => navigate("/auth/inventory")}>
+                        Back
+                    </button>
+                    <button className="btn-primary mr-auto" type="submit">
+                        Create
+                    </button>
+                </div>
+            </form>
         </div>
-        <div className="flex mt-6">
-          <button className="btn-secondary mr-4 ml-auto" type="submit"
-                  onClick={() => navigate("/auth/shipments")}>
-            Back
-          </button>
-          <button className="btn-primary mr-auto" type="submit">
-            Create
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+    );
 }
-
-export default NewShipment;
+    export default NewProduct;
