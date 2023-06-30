c8a24be81e7a281c136863614222ee78e674690a
Sebastian Bran <sebastianbranzapata@gmail.com>
2023-06-30 02:01:18 -0500

    [master]: fix build issue

--- src/pages/auth-views/shared/components/Navbar.tsx
+++ src/pages/auth-views/shared/components/Navbar.tsx
@@ -18,17 +18,23 @@ function Navbar() {
     toggleNavbar();
   };
 
-  return (
-    location.pathname !== "/auth/profile" &&
+  return location.pathname !== "/auth/profile" ? (
     <nav className="absolute lg:static lg:h-16 bg-gray-200 lg:bg-white flex lg:content-center flex-wrap w-full lg:w-fit h-full left-[-100%] left-0 transition-all lg:transition-none ease-in duration-500 menu-nav">
       <ul className="flex flex-col lg:flex-row w-full p-4 lg:p-0">
         {routes.map((route) => (
-          <NavLink className="lg:ml-4 text-base p-2 font-bold text-[#64748B] nav-link" to={route.path} key={route.name} onClick={handleNavlinkClick}>
+          <NavLink
+            className="lg:ml-4 text-base p-2 font-bold text-[#64748B] nav-link"
+            to={route.path}
+            key={route.name}
+            onClick={handleNavlinkClick}
+          >
             {route.name}
           </NavLink>
         ))}
       </ul>
     </nav>
+  ) : (
+    <></>
   );
 }
 
