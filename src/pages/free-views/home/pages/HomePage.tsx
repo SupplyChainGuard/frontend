432165d977b915cf8173059deacc7a235f8ce409
Sebastian Bran <sebastianbranzapata@gmail.com>
2023-06-30 03:44:59 -0500

    [master]: add home page content

--- src/pages/free-views/home/pages/HomePage.tsx
+++ src/pages/free-views/home/pages/HomePage.tsx
@@ -1,9 +1,17 @@
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+    <section className="home section" id="home">
+      <section className="fondo-con-texto">
+        <div className="contenido">
+          <h1>SupplyChainGuard</h1>
+          <p>
+            Control your inventory with the security and transparency of
+            blockchain technology
+          </p>
+        </div>
+      </section>
+    </section>
+  );
 }
 
 export default Home;
